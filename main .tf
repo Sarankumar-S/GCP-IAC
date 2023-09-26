bb699556d32862b249bb16e4d26ebcdbda94a7b6
Sarankumar S <saransekar94@gmail.com>
2023-09-26 15:22:42 +0530

    Create main .tf

---
+++ main .tf
@@ -0,0 +1,15 @@
+provider "google" {
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+}
